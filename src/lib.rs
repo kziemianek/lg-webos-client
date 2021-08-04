1b1ec6b2599b9f731187e54efa8dec9d0720f16a
GT3CH1 <gavinpease@gmail.com>
2021-08-04 01:31:44 -0600

    Fixup examples

--- src/lib.rs
+++ src/lib.rs
@@ -10,7 +10,7 @@
 //! #[tokio::main]
 //! async fn main() {
 //!     env_logger::init();
-//!     // Note: We must specify the ws protocol, and if we do not have the key, we must just use a blank str.
+//!     // Note: We must specify the ws protocol, and if we do not have the key, we must use a blank str.
 //!     let config = WebOsClientConfig::new("ws://192.168.1.62:3000/","");
 //!
 //!     let mut client = WebosClient::new(config).await.unwrap();
